23f2e5fb02e056ef3fdb07629dcb6986306de537
Roman <student.rv86@gmail.com>
2019-11-30 00:51:50 +0200

    add 2 & 3

---
+++ src/main/java/com/company/PartitionLinkedList.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.LinkedList;
+
+public class PartitionLinkedList {
+
+    public LinkedList<Integer> partitionList(LinkedList<Integer> list, int value) {
+        LinkedList<Integer> smaller = new LinkedList<Integer>();
+
+        if (list != null && list.size() != 0) {
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i) < value) {
+                    smaller.add(list.get(i));
+                    list.remove(i);
+                }
+                smaller.addAll(list);
+            }
+        }
+        return smaller;
+    }
+}
--- src/test/java/com/company/RotateLinkedListTest.java
+++ src/test/java/com/company/RotateLinkedListTest.java
@@ -1,27 +1,24 @@
 package com.company;
 
-import org.junit.Test;
-
-import java.util.Collections;
 import java.util.LinkedList;
-import java.util.List;
 
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class RotateLinkedListTest {
 
     @Test
-    public void testRotateLinkedList() {
+    public void testRotateList() {
         LinkedList<Integer> list = new LinkedList<Integer>();
         for (int i = 1; i <= 5 ; i++) {
             list.add(i);
         }
 
-        int[] expectedResult = {4, 5, 1, 2, 3};
-        RotateLinkedList rotateLinkedList = new RotateLinkedList();
+        Integer[] expectedResult = {4, 5, 1, 2, 3};
 
+        RotateLinkedList rotateLinkedList = new RotateLinkedList();
         LinkedList<Integer> result = rotateLinkedList.rotateList(list, 3);
 
-        //assertArrayEquals(result.toArray(), expectedResult);
+        assertArrayEquals(result.toArray(new Integer[result.size()]), expectedResult);
     }
 }
